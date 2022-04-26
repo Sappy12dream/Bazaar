572504b6665079145c76296993a7a5f2a2b9bdb6
sappy12dream <sapnaspy@gmail.com>
2022-04-26 10:59:15 +0530

    okay

--- frontend/src/Components/Main/Main.js
+++ frontend/src/Components/Main/Main.js
@@ -1,18 +1,21 @@
-import React, { useState } from 'react'
-import ProductScreen from '../ProductScreen/ProductScreen'
-import SubHeader from '../SubHeader/SubHeader'
-
+import React, { useState } from "react";
+import ProductScreen from "../ProductScreen/ProductScreen";
+import SubHeader from "../SubHeader/SubHeader";
 
 function Main() {
-  const [price, setprice] = useState()
-  const [ratings, setratings] = useState()
-  const [category, setcategory] = useState()
+  const [price, setprice] = useState();
+  const [ratings, setratings] = useState();
+  const [category, setcategory] = useState();
   return (
-    <div className='Main'>
-        <SubHeader  setprice={setprice} setratings={setratings} setcategory={setcategory}/>
-        <ProductScreen price={price} ratings={ratings} category={category}/>
+    <div className="Main">
+      <SubHeader
+        setprice={setprice}
+        setratings={setratings}
+        setcategory={setcategory}
+      />
+      <ProductScreen price={price} ratings={ratings} category={category} />
     </div>
-  )
+  );
 }
 
-export default Main
+export default Main;
